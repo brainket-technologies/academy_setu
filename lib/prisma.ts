35230a176a40cbbcc5625217692553200932dd6a
fammu8858 <mohdfahadit02@ggmail.com>
2026-07-27 03:15:10 +0530

    Fixed PrismaClient import and adapter configuration for dynamic dashboards

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma'
+import { PrismaClient } from '@/app/generated/prisma'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
